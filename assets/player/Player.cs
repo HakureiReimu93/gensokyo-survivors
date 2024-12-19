0f9719a247a6fdfe47ac657b0d19c3f29af9386f
HakureiReimu93 <henri@method93.com>
2024-12-19 10:04:02 +0900

    I forgor

--- assets/player/Player.cs
+++ assets/player/Player.cs
@@ -5,6 +5,8 @@ public partial class Player : CharacterBody2D
 {
     public override void _Ready()
     {
+        
+        // ()()()()()(:))
         string fatFuck = "youBitch";
         GD.Print(fatFuck);
 
