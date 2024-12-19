f2611153bcc8d3d7c6e5f113ae6c2d6516dcf3bd
HakureiReimu93 <henri@method93.com>
2024-12-19 10:31:42 +0900

    https://live.codetogether.io/#/10bceec1-c85e-4fbd-a744-e55c7a46bac9/8uDIKyDD7X7MBnFfUnTOer

--- assets/player/Player.cs
+++ assets/player/Player.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+
+    //https://live.codetogether.io/#/10bceec1-c85e-4fbd-a744-e55c7a46bac9/8uDIKyDD7X7MBnFfUnTOer
     public override void _Ready()
     {
         string fatFuck = "youBitch";
