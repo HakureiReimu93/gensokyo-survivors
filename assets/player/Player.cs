59302e85dbe50257a466ec8c66d1182ea131f2dc
HakureiReimu93 <henri@method93.com>
2024-12-19 10:28:02 +0900

    do this real quick

--- assets/player/Player.cs
+++ assets/player/Player.cs
@@ -5,15 +5,14 @@ public partial class Player : CharacterBody2D
 {
     public override void _Ready()
     {
-        
-    
-
-
-
-
-
         string fatFuck = "youBitch";
         GD.Print(fatFuck);
 
     }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+
 }
