e9a82b7d914163e8229f4d4acca5b6a7e42e431f
HakureiReimu93 <henri@method93.com>
2024-12-19 11:02:15 +0900

    https://live.codetogether.io/#/10bceec1-c85e-4fbd-a744-e55c7a46bac9/QqJLsMxyYTxEe4a9FLOPP4

--- assets/player/Player.cs
+++ assets/player/Player.cs
@@ -7,19 +7,22 @@ public partial class Player : CharacterBody2D
     //https://live.codetogether.io/#/10bceec1-c85e-4fbd-a744-e55c7a46bac9/8uDIKyDD7X7MBnFfUnTOer
     public override void _Ready()
     {
-        string fatFuck = "youBitch";
-        GD.Print(fatFuck);
-
     }
 
     public override void _Process(double delta)
     {
-        base._Process(delta);
+        var movementVector = GetMovementVector();
+        var direction = movementVector.Normalized();
     }
 
-    public override void get_movement_vector()
+    public Vector2 GetMovementVector()
     {
-        
+        var movementVector = Vector2.Zero;
+        var xMovement = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        var yMovement = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+
+        Vector2 vector = new Vector2(xMovement, yMovement);
+        return vector;
     }
 
 }
