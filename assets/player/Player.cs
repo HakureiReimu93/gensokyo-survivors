99bd250153183bc085b22a52a383dfd28f945c16
HakureiReimu93 <henri@method93.com>
2024-12-19 10:08:22 +0900

    💀💀💀

--- assets/player/Player.cs
+++ assets/player/Player.cs
@@ -5,7 +5,7 @@ public partial class Player : CharacterBody2D
 {
     public override void _Ready()
     {
-        
+        //Bro what 💀💀💀💀💀💀
         // ()()()()()(:))
         string fatFuck = "youBitch";
         GD.Print(fatFuck);
