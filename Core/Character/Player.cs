a7ccce5b00f32f3cb70b1df2efae4e84a0aabaa3
Pyrite32 <patrick@method93.com>
2025-05-30 09:54:30 -0700

    feat: restart project

---
+++ Core/Character/Player.cs
@@ -0,0 +1,19 @@
+using Godot;
+using static GodotStrict.Helpers.Calculate;
+using static GodotStrict.Helpers.Prelude;
+using static GodotStrict.Helpers.Guard.SafeGuard;
+using GodotStrict.Types;
+
+public partial class Player : CharacterBody2D
+{
+
+	
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+		float DELTA = (float)delta;
+	}
+}
--- interfaces/IBrain.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Godot;
-
-namespace GensokyoSurvivors.interfaces;
-
-public interface IBrain
-{
-    public Vector2 GetIntendedMovement();
-}
