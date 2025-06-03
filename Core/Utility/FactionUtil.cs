bf97bf8ffe60e612d4c1b3942ae450ba98371904
Pyrite32 <patrick@method93.com>
2025-06-03 16:13:46 -0700

    fix: edit hitbox+hurtbox to not use collision layer or mask

--- Core/Utility/FactionUtil.cs
+++ Core/Utility/FactionUtil.cs
@@ -4,16 +4,28 @@ namespace GensokyoSurvivors.Core.Utility;
 
 public static class FactionUtil
 {
-    private const UInt32 AllyLayerID = 0b0000_1000;
-    private const UInt32 EnemyLayerID = 0b0001_0000;
+  private const UInt32 AllyLayerID = 0b0000_1000;
+  private const UInt32 EnemyLayerID = 0b0001_0000;
 
-    public static UInt32 LayerFromFaction(FactionEnum factionValue)
+  public static UInt32 LayerFromFaction(FactionEnum factionValue)
+  {
+    return factionValue switch
+    {
+      FactionEnum.NONE => 0b0000_0000,
+      FactionEnum.ENEMY => EnemyLayerID,
+      FactionEnum.FRIEND => AllyLayerID,
+      FactionEnum.BOTH => EnemyLayerID | AllyLayerID,
+      _ => throw new NotImplementedException(),
+    };
+  }
+    
+    public static UInt32 MaskFromFaction(FactionEnum factionValue)
     {
         return factionValue switch
         {
           FactionEnum.NONE => 0b0000_0000,
-          FactionEnum.ENEMY => EnemyLayerID,
-          FactionEnum.FRIEND => AllyLayerID,
+          FactionEnum.ENEMY => AllyLayerID,
+          FactionEnum.FRIEND => EnemyLayerID,
           FactionEnum.BOTH => EnemyLayerID | AllyLayerID,
           _ => throw new NotImplementedException(),
         };
