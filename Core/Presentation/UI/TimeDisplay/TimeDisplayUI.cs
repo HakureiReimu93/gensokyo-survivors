d18ae265866af0a90ea59aca7b77f3cb75e72fab
Pyrite32 <patrick@method93.com>
2025-06-07 21:10:26 -0700

    feat: set up timer animations and integration

--- Core/Presentation/UI/TimeDisplay/TimeDisplayUI.cs
+++ Core/Presentation/UI/TimeDisplay/TimeDisplayUI.cs
@@ -2,40 +2,62 @@ using GensokyoSurvivors.Core.Interface.Lens;
 using GensokyoSurvivors.Core.Model;
 using GensokyoSurvivors.Core.Presentation.UI.TimeDisplay;
 using Godot;
+using GodotStrict.AliasTypes;
+using GodotStrict.Helpers;
 using GodotStrict.Helpers.Guard;
 using GodotStrict.Traits;
+using GodotStrict.Types;
 using GodotStrict.Types.PrevCurrent;
+using GodotUtilities;
 using System;
 
 namespace GensokyoSurvivors.Core.Presentation;
 
+[UseAutowiring]
 public partial class TimeDisplayUI : CanvasLayer, ILensProvider<LTimeLeftChannel>
 {
+	[Autowired]
+	Option<AnimationPlayer> mAnimation;
+
 	public override void _Ready()
 	{
-		SafeGuard.EnsureNotNull(MyTimeValueLabel);
+		__PerformDependencyInjection();
+
+
+		SafeGuard.EnsureNotNull(MyTimeLabel);
+		SafeGuard.EnsureNotNull(MyMillisLabel);
 
 		if (SessionSignalBus.SingletonInstance.Available(out var ssb))
 		{
 			ssb.SessionTimeExpired += HandleSessionTimeExpired;
 		}
 
-		MyTimeValueLabel.Text = "...";
+		MyTimeLabel.Text = "...";
+		MyMillisLabel.Text = "";
+
+		if (mAnimation.Available(out var anim))
+		{
+			SafeGuard.Ensure(anim.HasAnimation("pop-in"));
+			SafeGuard.Ensure(anim.HasAnimation("pop-out"));
+		}
 	}
 
 	protected void HandleSessionTimeExpired()
 	{
+		if (!mEverUpdatedBefore && mAnimation.Available(out var anim))
+		{
+			anim.Play("pop-out");
+		}
 	}
 
-	protected TimeDisplayUrgency CalculateUrgency(TimeBundle pTime)
+	protected static TimeDisplayUrgency CalculateUrgency(TimeBundle pTime)
 	{
-		var timeLeft = pTime.PercentTimeLeft();
 		// 5, 4, 3, 2, 1
 		if (pTime.TimeLeft <= 6f)
 		{
 			return TimeDisplayUrgency.Critical;
 		}
-		if (timeLeft < 0.2f)
+		if (pTime.PercentTimeLeft() < 0.2f)
 		{
 			return TimeDisplayUrgency.Mild;
 		}
@@ -45,33 +67,62 @@ public partial class TimeDisplayUI : CanvasLayer, ILensProvider<LTimeLeftChannel
 
 	protected void UpdateTime(TimeBundle pInput)
 	{
-		int minutes = Convert.ToInt32(pInput.TimeLeft / 60);
-		int seconds = Convert.ToInt32(pInput.TimeLeft % 60);
-
-		mSeconds.UpdateValue(seconds);
-
-		var urgency = CalculateUrgency(pInput);
-
-		if (mSeconds.CurrentNotEqualsPrevious() && urgency is not TimeDisplayUrgency.Normal)
+		if (!mEverUpdatedBefore && mAnimation)
 		{
-			// animate time down.
-			MyTimeValueLabel.SelfModulate = urgency switch
-			{
-				TimeDisplayUrgency.Normal => Colors.White,
-				TimeDisplayUrgency.Mild => Colors.Yellow,
-				TimeDisplayUrgency.Critical => Colors.Red,
-				_ => throw new NotImplementedException(),
-			};
-
+			mAnimation.Value.Play("pop-in");
+			mEverUpdatedBefore = true;
 		}
 
-		MyTimeValueLabel.Text = $"{minutes:D1}:{seconds:D2}";
+		int minutes = Convert.ToInt32(pInput.TimeLeft / 60);
+		int seconds = Convert.ToInt32(pInput.TimeLeft % 60);
+		int millis  = Convert.ToInt32((pInput.TimeLeft - seconds) * Math.Pow(10, 3));
+		var secondsFraction = Calculate.Fract(pInput.TimeLeft);
+
+
+		mSeconds.UpdateValue(seconds);
+		if (mSeconds.CurrentNotEqualsPrevious())
+		{
+			var urgency = CalculateUrgency(pInput);
+			mTextColorBase = urgency switch
+			{
+				TimeDisplayUrgency.Normal => Colors.White,
+				TimeDisplayUrgency.Mild => MyMildColor,
+				TimeDisplayUrgency.Critical => MyCriticalColor,
+				_ => throw new NotImplementedException(),
+			};
+			// animate time down.
+			if (mAnimation.Available(out var anim) &&
+				urgency != TimeDisplayUrgency.Normal)
+			{
+				anim.Play("pulse-value");
+			}
+		}
+
+		// Ease in curve, .
+		MyTimeLabel.Modulate = mTextColorBase.Lerp(
+			Colors.White,
+			Calculate.PhiEasing((normal)0.8f, secondsFraction));
+
+		MyTimeLabel.Text = $"{minutes:D2}:{seconds:D2}";
+		MyMillisLabel.Text = $"{millis:D3}";
 	}
 
 	[Export]
-	Label MyTimeValueLabel;
+	Label MyTimeLabel;
+
+	[Export]
+	Label MyMillisLabel;
+
+	[ExportCategory("Theme")]
+	[Export]
+	Color MyMildColor { get; set; } = Colors.Yellow;
+
+	[Export]
+	Color MyCriticalColor { get; set; } = Colors.Red;
 
 	PrevCurrentValue<int> mSeconds;
+	Color mTextColorBase = Colors.White;
+	bool mEverUpdatedBefore = false;
 
 	#region lens
 	private class TimeLeftChannel(TimeDisplayUI en) : LTimeLeftChannel
