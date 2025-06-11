a3a6d43751d07da514de557944549bc6dbe92e7b
Pyrite32 <patrick@method93.com>
2025-06-10 22:20:58 -0700

    refactor: test auto documentation

--- Core/Traits/CooldownAbilitySpawner.cs
+++ Core/Traits/CooldownAbilitySpawner.cs
@@ -7,39 +7,54 @@ using GensokyoSurvivors.Core.Interface;
 using System.Linq;
 using GodotUtilities;
 
+/// <summary>
+/// Spawns skills for the current player.
+/// </summary>
 [GlobalClass]
 [Icon("res://Assets/GodotEditor/Icons/factory.png")]
 [UseAutowiring]
 public partial class CooldownAbilitySpawner : Node, IAbilitySpawner
 {
-	[Autowired("id-skill-layer")]
-	Scanner<LMother> mSkillLayerRef;
+    /// <summary>
+    /// The layer upon which to spawn the ability
+    /// </summary>
+    [Autowired("id-skill-layer")]
+    private Scanner<LMother> mSkillLayerRef;
 
-	[Autowired]
-	IAdversarialUnitPicker mVictimPicker;
+    /// <summary>
+    /// The unit picker strategy node.
+    /// </summary>
+    [Autowired]
+    private IAdversarialUnitPicker mVictimPicker;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		__PerformDependencyInjection();
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        __PerformDependencyInjection();
 
-		SafeGuard.EnsureNotNull(MySkillToSpawn);
-		SafeGuard.EnsureFloatsNotEqual(MySpawnDelay, 0f, "delay cannot be 0");
-		SafeGuard.Ensure(Owner is Node2D, "Cannot take advantage of certain spawn strategies without Position reference");
+        SafeGuard.EnsureNotNull(MySkillToSpawn);
+        SafeGuard.EnsureFloatsNotEqual(MySpawnDelay, 0f, "delay cannot be 0");
+        SafeGuard.Ensure(Owner is Node2D, "Cannot take advantage of certain spawn strategies without Position reference");
 
-		//TODO: Ensure owner is a player, because this spawner only works on players.
-		mOwner = Owner as Node2D;
-	}
+        //TODO: Ensure owner is a player, because this spawner only works on players.
+        mOwner = Owner as Node2D;
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		if (mTimer.Tick(delta))
-		{
-			mTimer.Reset();
-			SpawnNext();
-		}
-	}
+    /// <summary>
+    /// Called every physics frame.
+    /// </summary>
+    public override void _PhysicsProcess(double delta)
+    {
+        if (mTimer.Tick(delta))
+        {
+            mTimer.Reset();
+            SpawnNext();
+        }
+    }
 
+	/// <summary>
+	/// Pick an antagonist and spawn the skill on the skill layer
+	/// </summary>
 	private void SpawnNext()
 	{
 		if (mSkillLayerRef.Unavailable(out var skillLayerInfo)) return;
@@ -71,29 +86,40 @@ public partial class CooldownAbilitySpawner : Node, IAbilitySpawner
 		skill.OnEnemyChosen(victim);
 	}
 
-	[Export]
-	PackedScene MySkillToSpawn { get; set; }
+    /// <summary>
+    /// The packed scene with the skill to spawn.
+    /// </summary>
+    [Export]
+    public PackedScene MySkillToSpawn { get; set; }
 
-	[Export(PropertyHint.Range, "0,10")]
-	float MySpawnDelay
-	{
-		get
-		{
-			return mSpawnDelay;
-		}
-		set
-		{
-			SafeGuard.Ensure(value > 0);
+    /// <summary>
+    /// The delay between spawns in seconds.
+    /// </summary>
+    [Export(PropertyHint.Range, "0,10")]
+    public float MySpawnDelay
+    {
+        get
+        {
+            return mSpawnDelay;
+        }
+        set
+        {
+            SafeGuard.Ensure(value > 0);
 
-			mTimer.ResetWithCustomTime(value);
-			mSpawnDelay = value;
-		}
-	}
-	float mSpawnDelay;
+            mTimer.ResetWithCustomTime(value);
+            mSpawnDelay = value;
+        }
+    }
 
-	Node2D mOwner;
-	LiteTimer mTimer;
+    private float mSpawnDelay;
 
-	
+    /// <summary>
+    /// The owner node.
+    /// </summary>
+    private Node2D mOwner;
 
+    /// <summary>
+    /// The timer for the spawn delay.
+    /// </summary>
+    private LiteTimer mTimer;
 }
