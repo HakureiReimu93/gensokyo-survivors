8425379dd2253229f2cc981b61baee669e523140
Pyrite32 <patrick@method93.com>
2025-06-10 22:18:34 -0700

    feat: enterbox is a detection hitbox that is not connected to damage

---
+++ Core/Traits/EnterBox.cs
@@ -0,0 +1,47 @@
+using Godot;
+using GensokyoSurvivors.Core.Utility;
+using GensokyoSurvivors.Core.Interface;
+using GensokyoSurvivors.Core.Utility.Adventure;
+using System;
+using GodotStrict.Types;
+using GodotUtilities;
+
+[GlobalClass]
+[Icon("res://Assets/GodotEditor/Icons/script.png")]
+[UseAutowiring]
+public partial class EnterBox : Area2D, IFactionMember
+{
+	[Autowired]
+	CollisionShape2D mCollisionShape;
+
+	[Signal]
+	public delegate void EnterBoxEnteredEventHandler();
+
+	public override void _Ready()
+	{
+		__PerformDependencyInjection();
+	}
+
+	public void SetEnabled(bool pEnabled)
+	{
+		mCollisionShape.Disabled = !pEnabled;
+	}
+
+	private void HandleAreaEntered(Area2D other)
+	{
+		if (other is HitBox otherHitBox &&
+			FactionUtil.DoFactionsOppose(otherHitBox.MyFaction, MyFaction))
+		{
+			EmitSignal(SignalName.EnterBoxEntered);
+		}
+	}
+
+	public void DoRespondToBoxEntered(Action with)
+	{
+		EnterBoxEntered += () => with();
+	}
+
+	[Export]
+	public FactionEnum MyFaction { get; set; }
+
+}
