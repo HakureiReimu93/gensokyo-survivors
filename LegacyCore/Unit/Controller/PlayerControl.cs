391f770451d808e2793213a74dd37dba103211f2
Pyrite32 <patrick@method93.com>
2025-06-16 15:59:58 -0700

    FEAT: move legacy assets

---
+++ LegacyCore/Unit/Controller/PlayerControl.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Linq;
+using GensokyoSurvivors.Core.Interface;
+using GensokyoSurvivors.Core.Interface.Lens;
+using GensokyoSurvivors.Core.Interface.Lens.Experience;
+using GensokyoSurvivors.Core.Model;
+using Godot;
+using GodotStrict.Helpers;
+using GodotStrict.Helpers.Guard;
+using GodotStrict.Types;
+using GodotStrict.Types.Coroutine;
+using GodotUtilities;
+
+[GlobalClass]
+[Icon("res://Assets/GodotEditor/Icons/brain.png")]
+[UseAutowiring]
+public partial class PlayerControl : Node, IMobUnitController, IPickupCommandReceiver
+{
+	[Autowired("chan-experience")]
+	Scanner<LExperienceChannel> mExperienceListeners;
+
+	[Autowired]
+	UpgradeLayer mUpgrades;
+
+	[Autowired("SkillRoster")]
+	Node mSkillRoster;
+
+	public override void _Ready()
+	{
+		__PerformDependencyInjection();
+
+		SafeGuard.Ensure(Owner is MobUnit mu);
+		mUnit = Owner as MobUnit;
+	}
+
+	public void OnControllerRequestDie(Action pEventHandler)
+	{
+		RequestDie += () => pEventHandler();
+	}
+
+	public Vector2 GetNormalMovement() => Input.GetVector("move_left",
+														  "move_right",
+															"move_up",
+														  "move_down");
+
+	public void ReceiveExpReward(uint pExperienceGainedRaw)
+	{
+		mCurrentExp += pExperienceGainedRaw;
+		if (mCurrentExp >= mMaxXp)
+		{
+			// will populate mLevel and mCurrentExp when called.
+			UpdateLevelAndExp(mCurrentExp, mLevel, out mLevel, out mCurrentExp);
+			mMaxXp = GetLevelUpExperienceRequirement(mLevel);
+
+			// TODO: handle multi-level up
+			var possibleUpgrades = mUpgrades.DoGetNextUpgrades_3();
+
+			if (IsInstanceValid(MyUpgradeSelectorPacked))
+			{
+				var ui = GensokyoSurvivorsSession.Instance.HostBlockingUIFromPacked<UpgradeSelectUi>(
+					MyUpgradeSelectorPacked,
+					out var unblockFunction
+				);
+
+				ui.ShowAndAwaitChoice(possibleUpgrades, unblockFunction)
+					.OnCompletedWithOutput(DoAddNewUpgrade);
+			}
+			else
+			{
+				DoAddNewUpgrade(
+					Calculate.RandomCollectionItem(possibleUpgrades)
+				);
+			}
+		}
+		if (mExperienceListeners.Available(out var expListeners))
+		{
+			expListeners.ReceiveExperience(new ExperienceBundle(
+				mCurrentExp,
+				mMaxXp,
+				mLevel
+			));
+		}
+	}
+
+	private void DoAddNewUpgrade(UpgradeMetaData pUpgrade)
+	{
+		if (pUpgrade.MyRewardScene != null &&
+			!mUpgrades.TryGetUpgradeMetaDataFrom(pUpgrade.MyID, out _))
+		{
+			mSkillRoster.AddChild(pUpgrade.MyRewardScene.Instantiate());
+		}
+
+		if (pUpgrade.MyDetails != null)
+		{
+			if (pUpgrade.MyDetails.TryGetValue(mAttackSpeedPath, out Variant val))
+			{
+				foreach (var spawner in mSkillRoster.GetChildren().Cast<CooldownAbilitySpawner>())
+				{
+					spawner.MySpawnDelay = Mathf.Max(1f, spawner.MySpawnDelay - val.AsSingle());
+				}
+			}
+			if (pUpgrade.MyDetails.TryGetValue(mMovementSpeedPath, out Variant val1))
+			{
+				mUnit.MyMaxSpeed *= val1.AsSingle();
+			}
+		}
+
+
+
+		mUpgrades.HostUpgrade(pUpgrade);
+	}
+
+	private static void UpdateLevelAndExp(uint currentExperience, uint currentLevel, out uint newLevel, out uint expRemainder)
+	{
+		var nextLevelRequirement = GetLevelUpExperienceRequirement(currentLevel);
+		while (nextLevelRequirement <= currentExperience)
+		{
+			currentLevel += 1;
+			currentExperience -= nextLevelRequirement;
+		}
+
+		newLevel = currentLevel;
+		expRemainder = currentExperience;
+	}
+
+	public void OnUnitDie()
+	{
+		// upload final experience to PostmortemSession 
+		if (ArenaSession.SingletonInstance is not null)
+		{
+			ArenaSession.SingletonInstance.TriggerDefeat();
+		}
+	}
+
+	public static uint GetLevelUpExperienceRequirement(uint level)
+	{
+		return cBaseLevelUpExpRequirement + (level * 20);
+	}
+
+	[Signal]
+	public delegate void RequestDieEventHandler();
+
+	[Export]
+	PackedScene MyUpgradeSelectorPacked { get; set; }
+
+	public Node Entity => this;
+
+	FuncAdventureSoon<UpgradeMetaData> mUpgradeAwaiter;
+	Vector2 mCalculatedMovement;
+	uint mCurrentExp = 0;
+	uint mMaxXp = GetLevelUpExperienceRequirement(1);
+	uint mLevel = 1;
+
+	StringName mMovementSpeedPath = new("player-movement-speed");
+	StringName mAttackSpeedPath = new("player-movement-speed");
+
+	MobUnit mUnit;
+
+	private const int cBaseLevelUpExpRequirement = (20);
+}
---
+++ LegacyCore/Unit/MobUnit.cs
@@ -0,0 +1,293 @@
+using Godot;
+using GensokyoSurvivors.Core.Interface;
+using GodotStrict.Types;
+using GodotUtilities;
+using GensokyoSurvivors.Core.Model;
+using GensokyoSurvivors.Core.Utility;
+using GensokyoSurvivors.Core.Interface.Lens;
+using GodotStrict.Helpers.Guard;
+using GodotStrict.Types.Locked;
+using GodotStrict.Traits;
+using GodotStrict.Types.Traits;
+using GodotStrict.Helpers.Logging;
+using GodotStrict.Helpers;
+using System;
+using System.Linq;
+using GodotStrict.Types.Coroutine;
+using System.Collections.ObjectModel;
+using GodotStrict.AliasTypes;
+
+[GlobalClass]
+[Icon("res://Assets/GodotEditor/Icons/unit.png")]
+[UseAutowiring]
+public partial class MobUnit : CharacterBody2D,
+	IKillable,
+	IPickupCommandDispatcher,
+	LInfo2D,
+	INewUpgradeDispatcher
+{
+	[Autowired]
+	UnitMonoVisual mVisuals;
+
+	[Autowired]
+	TakeDamageBuf mOnTakeDamageBufTemplate;
+
+	[Autowired]
+	Option<IMobUnitController> mUnitBrain;
+	// Principal velocity buf (which is acceleration in this case.)
+	[Autowired]
+	Option<IScalarMiddleware<Vector2>> mAccel;
+
+	[Autowired]
+	Option<HealthTrait> mHealth;
+
+	[Autowired]
+	Option<HurtBox> mHurtBox;
+
+	[Autowired("id-unit-layer")]
+	Scanner<LMother> mUnitLayerRef;
+
+	[Autowired("ReleaseOnDieLoot")]
+	Option<Node2D> mDropOnDeath;
+
+	[Autowired]
+	Option<Node> mSkillRoster;
+
+	[Autowired]
+	Option<HealthBar> mHealthBar;
+
+	[Signal]
+	public delegate void OnUnitDieEventHandler();
+
+	public override void _Ready()
+	{
+		__PerformDependencyInjection();
+
+		if (mHurtBox.Available(out var hurtBox))
+		{
+			hurtBox.MyTakeRawDamage += HandleHurtByDamageSource;
+		}
+		if (mHealth.Available(out var hp))
+		{
+			hp.MyHpDepleted += HandleHpDropToZero;
+		}
+
+		if (mUnitBrain.Available(out var controller))
+		{
+			controller.OnControllerRequestDie(TriggerDie);
+		}
+
+		if (mHealthBar.Available(out var hpBar))
+		{
+			hpBar.UpdatePercentage(new normal(1f));
+		}
+
+		mVisuals.DoRegisterFallbackAnim("idle")
+				.DoRegisterLoopingAnim("walk")
+				.DoRegisterFinalAnim("die")
+				.TryPlayAnimation("idle");
+	}
+
+	public void OnDie(Action pContinuation)
+	{
+		OnUnitDie += () => pContinuation();
+	}
+
+	private void SwitchToSessionOver(double delta)
+	{
+		// check faction. if enemy, just die.
+		if (!mHurtBox.Available(out var hb) &&
+			hb.MyFaction == FactionEnum.Enemy)
+		{
+			TriggerDie();
+		}
+	}
+
+	public void AddUnitBuf(UnitBuf ub)
+	{
+		MyBufs.Add(ub);
+		ub.OnUnitAddsMe(this);
+
+		foreach (var child in this.GetChildren())
+		{
+			if (child is INewBufSubject subject)
+			{
+				subject.ConsiderNewBuf(ub);
+			}
+		}
+	}
+
+	public void RemoveUnitBuf(UnitBuf ub)
+	{
+		MyBufs.RemoveSpecificUnitBuf(ub);
+		ub.OnUnitRemovesMe();
+
+		// Maybe this can fix leaked ObjectDB instances?
+		ub.QueueFree();
+	}
+
+	public override void _Process(double delta)
+	{
+		if (mDead) return;
+
+		// apply buf processing
+		MyBufs.ProcessAll(delta);
+
+		var moveDirection = mUnitBrain.MatchValue(
+			some: (val) => val.GetNormalMovement(),
+			none: () => Vector2.Zero
+		);
+
+		var finalVelocity = moveDirection * MyMaxSpeed;
+
+		if (mAccel.Available(out var accel))
+		{
+			finalVelocity *= accel.NextValue(moveDirection, delta);
+		}
+
+		// apply movement buf.
+		finalVelocity *= MyBufs.ProductAll(buf => buf.MyBaseMovementSpeedScale);
+
+		Velocity = finalVelocity;
+
+		// Talk to unit anim
+		if (moveDirection.IsZeroApprox() || finalVelocity.IsZeroApprox())
+		{
+			mVisuals.TryPlayAnimation("idle");
+		}
+		else
+		{
+			mVisuals.TryPlayAnimation("walk");
+		}
+
+		// apply color buf
+		Modulate = Colors.White * MyBufs.ColorMultiplyAll();
+
+		MoveAndSlide();
+	}
+
+	private void HandleHurtByDamageSource(float pRawDamage)
+	{
+		if (mHealth.Available(out var hp) && !mDead)
+		{
+			hp.TriggerDamage(pRawDamage);
+
+			Node duplicated = mOnTakeDamageBufTemplate.Duplicate();
+			SafeGuard.Ensure(duplicated is TakeDamageBuf);
+			TakeDamageBuf buf = duplicated as TakeDamageBuf;
+
+			if (hp.GetHealth().IsNegative() is false)
+			{
+				AddUnitBuf(buf);
+			}
+
+			if (mHealthBar.Available(out var healthBar))
+			{
+				healthBar.UpdatePercentage(
+					new normal(hp.GetHealth() / hp.GetMaxHealth())
+				);
+			}
+		}
+	}
+
+	private void HandleHpDropToZero(float pUnderflowRawDamage)
+	{
+		TriggerDie();
+	}
+
+	public void TriggerDie()
+	{
+		SafeGuard.Ensure(mDead.Never());
+		SafeGuard.Ensure(mDeathAnimCompetionStatus.IsLocked is false);
+
+		EmitSignal(SignalName.OnUnitDie);
+
+		MyBufs.OnDieAll();
+
+		if (mVisuals.TryPlayAnimationAndAwaitCompletion("die", out var soon) is Outcome.Succeed)
+		{
+			mDeathAnimCompetionStatus.LockTo(soon);
+			soon.OnCompleted(_Die);
+		}
+	}
+
+	/// <summary>
+	/// Final dead effects
+	/// </summary>
+	protected void _Die()
+	{
+		if (mUnitLayerRef.Available(out var unitLayer) &&
+			mDropOnDeath.Available(out var drops))
+		{
+			normal thisDropChance = new(drops.GetMeta("ThisDropChance", 0.5f).AsSingle());
+			if (Calculate.ChanceOf(thisDropChance))
+			{
+				foreach (var dropsChild in drops.GetChildren().Cast<Node2D>())
+				{
+					drops.RemoveChild(dropsChild);
+					unitLayer.TryHost(dropsChild);
+					dropsChild.GlobalPosition = GlobalPosition;
+				}
+			}
+		}
+		else
+		{
+			this.LogWarn("Could not find unitlayer");
+		}
+
+		if (mUnitBrain.Available(out var controller))
+		{
+			controller.OnUnitDie();
+		}
+
+		QueueFree();
+	}
+
+	public void TriggerDieForcely()
+	{
+		TriggerDie();
+	}
+
+	// Chain of responsibility that should send Exp to the player controller.
+	public Outcome SendExpReward(uint pExperienceGainedRaw)
+	{
+		if (mUnitBrain.IsSome &&
+			mUnitBrain.Value is not IPickupCommandReceiver pcr)
+		{
+			return Outcome.NoHandler;
+		}
+		else
+		{
+			var reciever = mUnitBrain.Value as IPickupCommandReceiver;
+			reciever.ReceiveExpReward(pExperienceGainedRaw);
+		}
+
+		return 0;
+	}
+
+	public void SendNewUpgrade(UpgradeMetaData pUpgrade, ReadOnlyDictionary<UpgradeMetaData, uint> pAllUpgrades)
+	{
+		SafeGuard.Ensure(mSkillRoster.IsSome);
+
+		var subjects = mSkillRoster.Value
+							.GetChildren()
+							.Cast<INewUpgradeSubject>();
+
+		foreach (var subject in subjects)
+		{
+			subject.ConsiderNewUpgrade(pUpgrade, pAllUpgrades);
+		}
+	}
+
+	[Export]
+	public float MyMaxSpeed { get; set; } = 200;
+
+	Lockable<AnimationCompletionSoon> mDeathAnimCompetionStatus;
+	TriggerFlag mDead;
+	public bool IsDead => mDead;
+	protected BufCollection MyBufs { get; set; } = new();
+
+	Node2D ILens<Node2D>.Entity => this;
+}
+
+
